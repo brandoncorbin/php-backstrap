b3206f11058e2a8dacd5740e7f59556ada8d9f29
fifthgear <bcorbin@infifthgear.com>
2013-09-18 12:00:40 -0400

    Cleaning up servicexy

--- service/index.php
+++ service/index.php
@@ -1,55 +1,38 @@
-<?
+<?php
+// Load Requires
 require '../vendor/autoload.php';
+use HybridLogic\Validation\Validator;
+use HybridLogic\Validation\Rule;
+
 $action = (array_key_exists("action", $_REQUEST)) ? $_REQUEST['action'] : "default";
+$action = strip_tags(trim(strtolower($action)));
 $inputs = $_REQUEST;
 
-header("Content-Type:application/json");
-switch($action) {
-
-	case "default" : 
-
-		$return = [
-			'success'=>true,
-			'message'=>'Welcome'
-		];
-
-	break; // default
-
-	case "cart" : 
-
-		$input = (array_key_exists("key", $inputs)) ? $inputs['key'] : null;
-
-		if($input==null) {
-			$return = [
-				'success' => false,
-				'message' => 'Missing key'
-			];
-		} else {
-		
-		}
-
-
-	break; // cart
-
-	case "test" : 
-
-		$return = [
-			'success'=>true,
-			'message'=>'Welcome'
-		];
-
-	break; // test
-
-	default : 
-
-		$return = [
-			'success'=>false,
-			'message'=>'Action not found'
-		];
-
-	break;
-
+// Check if a function exists for this request
+if(function_exists('res_'.$action)) {
+	// It exists, lets call it and pass the inputs
+	$return = call_user_func('res_'.$action, $inputs);
+} else {
+	// It doesn't exist, lets call the default
+	$return = req_default($inputs);
 }
 
+/***************************************
+Response Functions
+----------------------------------------
+url.com/service/?action=home 
+will trigger the res_home() function
+You can pass both GETS and POSTS to this page 
+and they will be mermged in to a $req object
+***************************************/
+
+function res_default($req) {
+	return [
+			'success'=>true,
+			'message'=>'Welcome'
+		];
+}
+
+header("Content-Type:application/json");
 echo json_encode($return);
 
